28f25958d4f02bfed4710d2e39a52106d399564c
Keisy312000 <kavalosartavia@gmail.com>
2022-07-28 21:18:26 -0600

    Finalización estructura de datos

---
+++ Code/app/src/main/java/com/example/p5_buses/Conexion.java
@@ -0,0 +1,81 @@
+package com.example.p5_buses;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class Conexion extends SQLiteOpenHelper {
+
+    public static final String DBname = "Proyecto.db";
+    public Conexion(Context context) {
+        super(context, "Proyecto.db", null, 1);  //creacion de la base de datos
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {  //creacion de tabla usuario
+        db.execSQL("create table usuarios(cedula int primary key,nombre text,apellidos text,email text,usuario text,contraseña text,rol text)");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+        db.close();
+
+    }
+
+
+    public void registrousuarios() {  // insercion de usuarios
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        db.execSQL("INSERT OR IGNORE INTO  usuarios  (cedula ,nombre ,apellidos ,email ,usuario ,contraseña ,rol ) Values (70280733,'Keisy','Avalos Artavia','kavalosartavia@gmail.com','Keisy31','Camino123','Administrador')");
+        db.execSQL("INSERT OR IGNORE INTO usuarios (cedula ,nombre ,apellidos ,email ,usuario ,contraseña ,rol) Values (72800744,'Rosa','Hernandez Trejos','Rosamaria@gmail.com','Rosa27','Patito123','Usuario')");
+        db.close();
+
+    }
+
+    public boolean exiteusuario(String usuario, String contrasena) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from usuarios where usuario=? and  contraseña=?", new String[]{usuario, contrasena});
+        if (cursor.getCount() > 0){
+            return true;
+        }
+
+          else{
+              db.close();
+        }
+
+            return false;
+    }
+
+
+    public boolean tipousuario(String usuario) {
+
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select  rol  from usuarios where usuario=?", new String[]{usuario});
+
+
+        if (cursor.moveToFirst()) {
+            if (cursor.getString(cursor.getColumnIndexOrThrow("rol")).equals("Administrador"))
+
+                return  true;
+
+
+        }else{
+            db.close();
+        }
+
+
+        return false;
+    }
+
+
+
+
+
+
+}
--- Code/app/src/main/java/com/example/p5_buses/MainActivity.java
+++ Code/app/src/main/java/com/example/p5_buses/MainActivity.java
@@ -3,12 +3,68 @@ package com.example.p5_buses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Conexion conexion;
+    private EditText et1usuario, et2contraseña;
+    String usuario,contraseña;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1usuario = findViewById(R.id.et1usuario);
+        et2contraseña = findViewById(R.id.et2contraseña);
+        conexion = new Conexion(this);
+        conexion.registrousuarios();
+
     }
+
+    public void ingresar(View view) {
+
+        usuario = et1usuario.getText().toString();
+        contraseña = et2contraseña.getText().toString();
+
+
+        if (usuario == null || "".equals(contraseña)) {
+
+            Toast.makeText(this, "campos invalidos", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            boolean existe = conexion.exiteusuario(usuario, contraseña);
+
+            if (existe) {
+                boolean tipousuario = conexion.tipousuario(usuario);
+
+                if (tipousuario) {
+ //ingresar ventana administrador
+                    Toast.makeText(this, "Bienvenido administrador", Toast.LENGTH_SHORT).show();
+
+                } else {
+//ingresar venta usuario
+                    Toast.makeText(this, "Bienvenido usuario", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            } else {
+                Toast.makeText(this, "credenciales incorrectos", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+        et1usuario.setText("");
+        et2contraseña.setText("");
+
+    }
+
+
+
+
 }
+
