076f1948777873c8cd0c97e2ae31a9a78ff38750
keysiquiros <108992564+keysiquiros@users.noreply.github.com>
2022-08-05 17:23:07 -0600

    FE mantenimiento usuarios

--- Code/app/src/main/java/com/example/p5_buses/MenuAdmin.java
+++ Code/app/src/main/java/com/example/p5_buses/MenuAdmin.java
@@ -2,7 +2,9 @@ package com.example.p5_buses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuAdmin extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MenuAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+    }
+    public void irMantenimientoUsuarios(View view) {
+        Intent intent = new Intent(MenuAdmin.this, CrudUsuarios.class);
+        startActivity(intent);
     }
 }
